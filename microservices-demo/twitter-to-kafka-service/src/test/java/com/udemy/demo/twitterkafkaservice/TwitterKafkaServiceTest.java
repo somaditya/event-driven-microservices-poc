9a1737e38f1fb22fff91300dc1ecfd805a399de7
Somaditya Basak <mail@somaditya.com>
2023-12-27 02:48:13 +0530

    add module

---
+++ microservices-demo/twitter-to-kafka-service/src/test/java/com/udemy/demo/twitterkafkaservice/TwitterKafkaServiceTest.java
@@ -0,0 +1,14 @@
+package com.udemy.demo.twitterkafkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class TwitterKafkaServiceTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
